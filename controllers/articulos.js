1ddfa1571f5c33d01968af57fe634458ae29266c
Jesus Garcia <jesgarv@gmail.com>
2022-05-29 16:30:03 -0500

    gestion de articulos

--- controllers/articulos.js
+++ controllers/articulos.js
@@ -1,18 +1,9 @@
 const util = require("../util");
 const router = require("express").Router();
 
-router.post("/gestion", (req,res)=>{
-    /*
+router.post("/gestion",util.agregaTokenPeticion ,(req,res)=>{
     // Verifica la sesión para no dar acceso a usuarios no loggeados
-    if (!req.session.id_usuario) {
-        return res.json({
-            message: "Debe tener una sesión activa",
-            error: false
-        });
-    }
-
-    // Verifica que el tipo de usuario sea el correcto, en este caso el líder de comite
-    if (req.session.tipo != 3)
+    if (!(req.decoded.tipo == 6 || req.decoded.tipo == 3))
         return res.json({
             message: "Permisos denegados",
             error: false
@@ -21,54 +12,38 @@ router.post("/gestion", (req,res)=>{
 
     // Consulta a la base de datos
     const query = "SELECT * FROM vw_articulo_version_autor";
-
-    connection.query(query, (err, results) => {
-        if (err)
-            return res.json({
-                message: err.message,
-                error: true
-            });
-
-        if (results.length > 0)
+    
+    util.createConnection((errorCon, con)=>{
+        if(errorCon){
             res.json({
-                message: "Artículos encontrados",
-                error: false,
-                data: results
-            });
-        else
-            res.json({
-                message: "No hay resultados que mostrar",
-                error: false
+                status: 500,
+                error : true,
+                message : "Ha ocurrido un error"
             })
-    });*/
-    const respuesta = {
-        error: false,
-        message: "To cool",
-        data : [
-            {
-                id_usuario : 1,
-                autor : "Jesús García",
-                id_articulo : 1,
-                titulo_articulo : "Articulo guay",
-                id_version : 1,
-                archivo : "/",
-                num_version : 1,
-                estatus : "En revisión"
-            },
-            {
-                id_usuario : 2,
-                autor : "Alberto Valencia",
-                id_articulo : 2,
-                titulo_articulo : "Articulo guay 2",
-                id_version : 2,
-                archivo : "/",
-                num_version : 1,
-                estatus : "Aceptado"
-            }
-        ]
-    }
-
-    res.json(respuesta);
+        }
+        
+        con.query(query, (err, results) => {
+            if (err)
+                return res.json({
+                    message: err.message,
+                    error: true
+                });
+    
+            if (results.length > 0)
+                res.json({
+                    message: "Artículos encontrados",
+                    error: false,
+                    data: results
+                });
+            else
+                res.json({
+                    message: "No hay resultados que mostrar",
+                    error: false
+                })
+        });
+    })
+    
+    
 
 });
 
